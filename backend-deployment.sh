729203e8a4098238ce30f3d553773d7c98150c6b
Amishak14 <116254472+Amishak14@users.noreply.github.com>
2023-02-09 16:23:56 +0530

    Update backend-deployment.sh

--- backend-deployment.sh
+++ backend-deployment.sh
@@ -1,3 +1,3 @@
 oc login --token="$OC_TOKEN" --server="$OC_SERVER"
 oc project "$PROJECT_NAME"
-oc apply -f manifest.yaml   
+oc apply -f https://github.com/Amishak14/tag-image/my-folder/manifest.yaml.git  
